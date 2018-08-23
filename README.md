6c83d687f3fa4957ac0e131708607fc39110d96d
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-22 23:35:30 -0700

    Basic skeleton of project.

--- README.md
+++ README.md
@@ -1,6 +1,46 @@
 # High-order Solution Transfer between Curved Meshes
 
-This is a paper with Per-Olof Persson based on work towards my
-PhD [thesis][1].
+This is work with Per-Olof Persson.
+
+This originally appeared in my [thesis][1] and is now split out
+into a standalone [paper][3].
+
+This repository is laid out in a manner described in
+[Good Enough Practices in Scientific Computing][2].
+
+## Abstract
+
+The problem of solution transfer between meshes arises frequently in
+computational physics, e.g. in Lagrangian methods where remeshing
+occurs. The interpolation process must be conservative, i.e. it
+must conserve physical properties, such as mass. We extend previous
+works --- which described the solution transfer process for straight sided
+unstructured meshes --- by considering high-order isoparametric meshes
+with curved elements. The implementation is highly reliant on accurate
+computational geometry routines for evaluating points on and
+intersecting B&#xe9;zier curves and triangles.
+
+## Installation
+
+The code used to build the manuscript, generate images and verify
+computations is written in Python. To run the code, Python 3.7
+should be installed, along with ``nox-automation``:
+
+```
+python -m pip install --upgrade nox-automation
+```
+
+Once installed, the various build jobs can be listed. For example:
+
+```
+$ nox --list-sessions
+Available sessions:
+* build_tex
+```
+
+To run ``nox -s build_tex`` (i.e. to build the PDF), ``pdflatex`` and
+``bibtex`` are required.
 
 [1]: https://github.com/dhermes/phd-thesis
+[2]: https://arxiv.org/abs/1609.00037
+[3]: doc/paper.pdf
---
+++ nox.py
@@ -0,0 +1,82 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This is a configuration file for running ``nox`` on this project.
+
+To determine the supported actions run ``nox --list-sessions`` from the
+project root.
+"""
+
+import os
+
+import nox
+import py.path
+
+
+NOX_DIR = os.path.abspath(os.path.dirname(__file__))
+SINGLE_INTERP = "python3.7"
+
+
+def get_path(*names):
+    return os.path.join(NOX_DIR, *names)
+
+
+class Remove(object):
+    def __init__(self, prefix, extensions):
+        self.prefix = prefix
+        self.extensions = extensions
+
+    def __call__(self):
+        for extension in self.extensions:
+            path = "{}.{}".format(self.prefix, extension)
+            os.remove(path)
+
+
+def build_tex_file(session, base, new_id, extensions=()):
+    # NOTE: This assumes that ``session.chdir(get_path('doc'))``
+    #       has been called.
+    modify_id = get_path("scripts", "modify_pdf_id.py")
+
+    session.run("pdflatex", base)
+    session.run("bibtex", base)
+    session.run("pdflatex", base)
+    session.run("bibtex", base)
+    session.run("pdflatex", base)
+    session.run("pdflatex", base)
+
+    path = get_path("doc", base)
+    remove = Remove(path, extensions)
+    session.run(remove)
+    session.run("python", modify_id, "--base", path, "--id", new_id)
+
+
+@nox.session
+def build_tex(session):
+    session.interpreter = SINGLE_INTERP
+
+    if py.path.local.sysfind("pdflatex") is None:
+        session.skip("`pdflatex` must be installed")
+
+    if py.path.local.sysfind("bibtex") is None:
+        session.skip("`bibtex` must be installed")
+
+    # No need to create a virtualenv.
+    session.virtualenv = False
+
+    session.chdir(get_path("doc"))
+
+    build_tex_file(
+        session,
+        "paper",
+        "5178018C9121C4981906E150C72BEC1B",
+        extensions=("aux", "bbl", "blg", "log", "out", "toc"),
+    )
